23e372184c82ad61bb26e109c4bff9c554458b04
Leonardo Veras <verasleonardo210@gmail.com>
2024-09-04 22:25:11 -0300

    ✨ feat: add error handler

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from task.routers import tasks
+from shared.exceptions import NotFound
+from shared.exceptions_handler import not_found_exception_handler
 
 
 app = FastAPI()
 app.router.include_router(tasks.router)
+app.add_exception_handler(NotFound, not_found_exception_handler)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
